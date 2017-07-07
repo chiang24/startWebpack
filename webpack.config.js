05435fad90b260c3e7fa4439dfc63cae00411d0d
chiang24 <438085855@qq.com>
2017-07-07 12:57:48 +0800

    HTML自动生成、JS代码压缩

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,31 @@
 var path = require('path');
+var webpack = require('webpack');
+var uglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
 
 module.exports = {
   //入口
   entry: './src/index.js',
   //输出
   output: {
-        filename: 'bundle.js',
+        filename: 'bundle-[hash].js',
 	path: path.resolve(__dirname, 'dist')
    },
+  plugins: [
+        new uglifyJsPlugin({
+	compress: {
+	warnings: false
+		       }
+        }),
+	    new HtmlwebpackPlugin({
+	          title: 'Webpack-demos',
+		        filename: 'index.html'
+			    }),
+			        new OpenBrowserPlugin({
+				      url: 'http://localhost:8080'
+				          })
+     ],
     module: {
         loaders:[
 	  { 
@@ -25,7 +43,15 @@ module.exports = {
 	                 'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
 			 'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false'
 	     ]
-	  }
-   ]
-  }
-};
+	  },
+        {
+	      test: /\.jsx?$/,
+	      exclude: /(node_modules|bower_components)/,
+	      loader: 'babel-loader', // 'babel-loader' is also a legal name to reference
+	      query: {
+	      presets: ['es2015']
+	                 }
+	}
+      ]
+   }   
+}
